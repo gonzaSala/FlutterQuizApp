397bdd41eb0d19515a7b5da0d21660435e305354
gonzaSala <gonzasalamendi@gmail.com>
2023-09-19 22:01:35 -0300

    en preceso, parte de buttons

--- lib/questionModel.dart
+++ lib/questionModel.dart
@@ -21,5 +21,27 @@ List<question> getQuestions() {
     answer('0,5 Kg', false),
     answer('100 Kg', false),
   ]));
+
+  list.add(question('Cuantos son 100 gramos?', [
+    answer('1 Kg', false),
+    answer('10 Kg', false),
+    answer('0,1 Kg', true),
+    answer('100 Kg', false),
+  ]));
+
+  list.add(question('Cuantos son 10 gramos?', [
+    answer('1 Kg', false),
+    answer('10 Kg', false),
+    answer('0,01 Kg', true),
+    answer('100 Kg', false),
+  ]));
+
+  list.add(question('Cuantos son 10000 gramos?', [
+    answer('1 Kg', false),
+    answer('10 Kg', true),
+    answer('0,5 Kg', false),
+    answer('100 Kg', false),
+  ]));
+
   return list;
 }
--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_simple_quiz_app/questionModel.dart';
 
 class QuizScreen extends StatefulWidget {
   @override
@@ -6,6 +9,13 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
+  //definimos los datos
+
+  List<question> questionList = getQuestions();
+  int currentQuestionIndex = 0;
+  int score = 0;
+  answer? selectedAnswer;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,13 +32,82 @@ class _QuizScreenState extends State<QuizScreen> {
               ),
             ),
             questionWidget(),
+            answerList(),
           ],
         ),
       ),
     );
   }
-}
 
-questionWidget() {
-  return Column();
+  questionWidget() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Questios ${currentQuestionIndex + 1}/${questionList.length.toString()}",
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Container(
+          width: double.infinity,
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(32),
+          decoration: BoxDecoration(
+              color: Colors.orange, borderRadius: BorderRadius.circular(15)),
+          child: Text(
+            questionList[currentQuestionIndex].questionText,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  answerList() {
+    return Column(
+      children: questionList[currentQuestionIndex]
+          .answerList
+          .map(
+            (e) => answerButton(e),
+          )
+          .toList(),
+    );
+  }
+
+  Widget answerButton(answer Answer) {
+    bool isSelected = Answer == selectedAnswer;
+
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      height: 48,
+      child: ElevatedButton(
+        child: Text(Answer.answerText),
+        style: ElevatedButton.styleFrom(
+          shape: const StadiumBorder(),
+          primary: isSelected ? Colors.orangeAccent : Colors.white,
+          onPrimary: isSelected ? Colors.white : Colors.black,
+        ),
+        onPressed: () {
+          if (selectedAnswer == null) {
+            if (Answer.isCorrect) {
+              score++;
+            }
+            setState(() {
+              selectedAnswer = Answer;
+            });
+          }
+        },
+      ),
+    );
+  }
 }
